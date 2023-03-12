2eba1852e2e8ca1cf0941e3d1eec753726cff15e
Myriam JAYLE <myriamjayle@gmail.com>
2023-03-12 18:52:55 +0100

    carroussel fini

--- assets/script.js
+++ assets/script.js
@@ -19,23 +19,52 @@ const slides = [
 
 const arrow_left = document.querySelector(".arrow_left");
 const arrow_right = document.querySelector(".arrow_right");
+const nbSlides = slides.length;
 
-arrow_left.addEventListener("click", function() {          
-    alert("CLICK!!!"); 
-});
+const bannerImage = document.querySelector(".banner-img");
+bannerImage.src = "./assets/images/slideshow/" + slides[0].image;
 
-arrow_right.addEventListener('click', function() {          
-    alert("CLICK!!!"); 
-});
+const paraf = document.querySelector("#banner p");
+paraf.innerHTML = slides[0].tagLine;
 
 let dots = document.querySelector(".dots");
 
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < nbSlides; i++) {
 	const point = document.createElement("div");
 	dots.appendChild(point);
 	point.classList.add("dot");	
 }
 
-let selected = document.querySelector(".dot");
-selected.classList.add("dot_selected");	
+dotNb = document.querySelectorAll(".dot");
+dotNb[0].classList.add("dot_selected");	
 
+arrow_left.addEventListener("click", clicleft);
+
+arrow_right.addEventListener('click', clicright);
+
+function clicright() {
+	indice = 0;
+	for (let i = 0; i < nbSlides; i++) {
+		test = dotNb[i].classList;
+		if (test.contains("dot_selected")) {indice=i;};	
+	}
+	dotNb[indice].classList.remove("dot_selected");
+	if (indice==3) { indice=0; } else { indice++;};
+	dotNb[indice].classList.add("dot_selected");
+	bannerImage.src = "./assets/images/slideshow/" + slides[indice].image;
+	paraf.innerHTML = slides[indice].tagLine;
+};
+
+function clicleft() {          
+	indice = 0;
+	for (let i = 0; i < nbSlides; i++) {
+		test = dotNb[i].classList;
+		if (test.contains("dot_selected")) {indice=i;};	
+	}
+	console.log (indice);
+	dotNb[indice].classList.remove("dot_selected");
+	if (indice==0) { indice=3; } else { indice--;};
+	dotNb[indice].classList.add("dot_selected");	
+	bannerImage.src = "./assets/images/slideshow/" + slides[indice].image;
+	paraf.innerHTML = slides[indice].tagLine;
+};
